f487e768f9c66859d5fcf2c9b9c8e39780ca6fad
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-02-19 11:46:36 +0530

    2nd commit
    Directory has been changed

--- PrintLinksCountLimitWebElement.java
+++ PrintLinksCountLimitWebElement.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.interactions.Actions;
 public class PrintLinksCountLimitWebElement {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver","C:\\\\chromedriver_win32\\\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		
