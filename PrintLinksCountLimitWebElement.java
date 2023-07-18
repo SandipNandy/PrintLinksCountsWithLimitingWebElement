08d66f751af5a513660eeda10b9328e06a84d1f9
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-18 11:26:31 +0530

    Update PrintLinksCountLimitWebElement.java

--- PrintLinksCountLimitWebElement.java
+++ PrintLinksCountLimitWebElement.java
@@ -7,11 +7,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-
-
 public class PrintLinksCountLimitWebElement {
-
-	public static void main(String[] args) throws InterruptedException {
+      public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
