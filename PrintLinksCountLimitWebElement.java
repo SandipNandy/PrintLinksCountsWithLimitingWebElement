2de0f19a34be0349cb4ecc354a42de241f055466
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-16 10:49:08 +0530

    Update PrintLinksCountLimitWebElement.java

--- PrintLinksCountLimitWebElement.java
+++ PrintLinksCountLimitWebElement.java
@@ -29,6 +29,7 @@ public class PrintLinksCountLimitWebElement {
 		Set<String> GetTabTitle = driver.getWindowHandles();
 		
 		Iterator<String> itr = GetTabTitle.iterator();
+		
 		driver.switchTo().window(itr.next());
 		while(itr.hasNext()) {
 			Thread.sleep(2000);
