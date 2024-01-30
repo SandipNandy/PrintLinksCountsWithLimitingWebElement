cfef2b9779156c874ec4fa29271c051d0c54a964
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-01-30 14:21:21 +0530

    Update PrintLinksCountLimitWebElement.java

--- PrintLinksCountLimitWebElement.java
+++ PrintLinksCountLimitWebElement.java
@@ -13,7 +13,6 @@ public class PrintLinksCountLimitWebElement {
 		
 	      System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
 	       WebDriver driver=new ChromeDriver();
-		
 	       driver.manage().window().maximize();
 		
 	       driver.get("https://AutomationPractice/");
